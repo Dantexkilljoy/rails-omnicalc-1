7da388161a057d6707a653797480f4df71674a1b
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-09-05 15:48:05 +0000

    added calculator routes and square results route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  
+  root to: "calculator#index"
+
+  get "square/new", to: "calculator#index"
+
+  get "square_root/new", to: "calculator#square_root"
+
+  get "payment/new", to: "calculator#payment"
+
+  get "random/new", to: "calculator#random"
+
+  get "square/results", to: "calculator#square_results"
+
+
+
+  # get "results"
 end
